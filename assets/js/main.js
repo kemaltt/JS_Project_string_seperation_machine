54eeed563f6f30bd59b803e696222a7f6445064a
kemaltt <ttnckemal@gmail.com>
2022-05-19 16:36:52 +0200

    alert adding

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,29 +7,30 @@ let resultHinterer = document.getElementById('output-hinterer')
 
 const absenden = () => {
     let zeichenkette = document.getElementById('input-text').value
-        // console.log(zeichenkette);
     let trennungsPosition = document.getElementById('input-text2').value
     let indexTrennunsPosition = zeichenkette.indexOf(trennungsPosition);
     let vordererTeil;
     let hintererTeil;
+    if (zeichenkette == '' || trennungsPosition == '') {
+        alert('Gib bitte einen Wert ein!')
+        resultVorderer.innerHTML = '';
+        resultHinterer.innerHTML = '';
+    } else {
 
-
-    if (indexTrennunsPosition !== -1) {
-        if (radioDavor.checked) {
-            vordererTeil = zeichenkette.slice(0, indexTrennunsPosition)
-            hintererTeil = zeichenkette.slice(indexTrennunsPosition)
-        } else {
-            vordererTeil = zeichenkette.slice(0, indexTrennunsPosition + trennungsPosition.length)
-            hintererTeil = zeichenkette.slice(indexTrennunsPosition + trennungsPosition.length)
+        if (indexTrennunsPosition !== -1) {
+            if (radioDavor.checked) {
+                vordererTeil = zeichenkette.slice(0, indexTrennunsPosition)
+                console.log(vordererTeil);
+                hintererTeil = zeichenkette.slice(indexTrennunsPosition)
+            } else {
+                vordererTeil = zeichenkette.slice(0, indexTrennunsPosition + trennungsPosition.length)
+                hintererTeil = zeichenkette.slice(indexTrennunsPosition + trennungsPosition.length)
+            }
+        } else if (radioDanach.checked) {
+            vordererTeil = 'Fehler!!!'
+            hintererTeil = zeichenkette;
         }
-    } else if (radioDanach.checked) {
-        vordererTeil = 'Fehler!!!'
-        hintererTeil = zeichenkette;
+        resultVorderer.innerHTML = vordererTeil;
+        resultHinterer.innerHTML = hintererTeil;
     }
-
-    // console.log(vordererTeil);
-    // console.log(hintererTeil);
-    resultVorderer.innerHTML = vordererTeil;
-    resultHinterer.innerHTML = hintererTeil;
-
 }
